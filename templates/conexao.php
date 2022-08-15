dcca5ec60c260a50a39475f67fd10cd1b894a576
Eduardo Alves <eduardoalvesalmeida1@gmail.com>
2022-08-14 22:09:07 -0300

    alt banco

--- templates/conexao.php
+++ templates/conexao.php
@@ -1,8 +1,7 @@
 <?php
-	try{
-		$conexao = new PDO('mysql:host=localhost;dbname=ep2018_escola_db_cms','ep201_root','3scol@2018');
-		$conexao->exec("set names utf8");
-	}catch(PDOException $e){
-		echo "Erro ao conectar com o banco de dados";
-	}
-?>
+try {
+	$conexao = new PDO('mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_a70e0523a5be900', 'b4f8af815d73ba', '07308cf8');
+	$conexao->exec("set names utf8");
+} catch (PDOException $e) {
+	echo "Erro ao conectar com o banco de dados";
+}
